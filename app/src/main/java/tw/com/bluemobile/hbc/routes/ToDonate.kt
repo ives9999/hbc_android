aac611b8586e3ba3ec95f01907df511d79461d96
ives <ives@bluemobile.com.tw>
2023-08-23 14:55:46 +0800

    1.0.0 is end.

--- app/src/main/java/tw/com/bluemobile/hbc/routes/ToDonate.kt
+++ app/src/main/java/tw/com/bluemobile/hbc/routes/ToDonate.kt
@@ -1,13 +1,15 @@
 package tw.com.bluemobile.hbc.routes
 
 import android.content.Intent
+import android.net.Uri
 import tw.com.bluemobile.hbc.controllers.BaseActivity
 import tw.com.bluemobile.hbc.controllers.DonateActivity
 
 interface ToDonate {
 
     fun toDonate(activity: BaseActivity) {
-        val i = Intent(activity, DonateActivity::class.java)
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://donate.newebpay.com/catpool/20190414"))
+        //val i = Intent(activity, DonateActivity::class.java)
         activity.startActivity(i)
     }
 }
